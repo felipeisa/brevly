29bf8367af9b0a8ed312cac5beb64bb6fba40300
Felipe <felipealves.081991@gmail.com>
2026-09-19 10:33:22 -0300

    new: http com fastify

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,12 +1,38 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
-import { env } from '@/env'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { createLinkRoute } from './routes/create-link'
 
 const server = fastify()
 
+// Validacao e Serializacao
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+// Error handler Global
+server.setErrorHandler((error, request, reply) => {
+  // identifica se e um erro de validacao
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.validation,
+    })
+  }
+
+  // Envia o erro p/ alguma ferramenta de observabilidade (Sentry/Datadog/Grafana/Otel)
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Erro do servidor interno' })
+})
+
 server.register(fastifyCors, { origin: '*' })
 
-console.log(env.DATABASE_URL)
+server.register(createLinkRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server Executando')
